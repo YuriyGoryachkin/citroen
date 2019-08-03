418e896ad8f77acbca2f570d56bb482e09466a91
yuriy-msi <monguste@gmail.com>
2019-08-03 20:35:58 +0300

    modify

--- test.py
+++ test.py
@@ -1,9 +1,31 @@
 import obd
+import os
+import time
+
+def clear():
+    os.system('clear')
 
 # connection = obd.OBD()  # auto-connects to USB or RF port
 ports = obd.scan_serial()  # return list of valid USB or RF ports
 print(ports)  # ['/dev/ttyUSB0', '/dev/ttyUSB1']
+time.sleep(2)
 connection = obd.OBD()
+# print('1')
+# time.sleep(1)
+# clear()
 cmd = obd.commands.RPM  # select an OBD command (sensor)
 response = connection.query(cmd)  # send the command
-print(response)  # "2410 RPM"
+# print('2')
+# time.sleep(1)
+# clear()
+# print('rpm: {}'.format(response))  # "2410 RPM"
+while connection:
+    clear()
+    cmd = obd.commands.RPM  # select an OBD command (sensor)
+    response = connection.query(cmd)  # send the command
+    clear()
+    time.sleep(0.5)
+    print('rpm: {}'.format(response))  # "2410 RPM"
+    time.sleep(0.5)
+    print('rere')
+    time.sleep(0.5)
